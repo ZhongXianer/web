d20d78689450ecf86a7f92ba16c87b195f198d8b
zhusiyi <xiaosiyier@163.com>
2020-03-20 11:50:25 +0800

    first

---
+++ app/src/main/java/com/example/a20200320/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.a20200320;
+import java.io.File;
+import java.io.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
+
+import android.os.Environment;
+import	android.provider.MediaStore;
+
+import android.Manifest;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.muddzdev.quickshot.QuickShot;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private WebView mWebView;
+    private Bitmap shotWeb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView=(WebView)findViewById(R.id.mWeb);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl("http://www.baidu.com");
+        askPermissions();
+        shotWeb=ShotWeb();
+        saveImageToGallery(shotWeb);
+
+    }
+
+
+    public void saveImageToGallery(Bitmap bitmap) {
+        // 首先保存图片
+        File file = null;
+        String fileName = System.currentTimeMillis() + ".jpg";
+        File root = new File(Environment.getExternalStorageDirectory(), getPackageName());
+        File dir = new File(root, "images");
+        if (dir.mkdirs() || dir.isDirectory()) {
+            file = new File(dir, fileName);
+        }
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //其次把文件插入到系统图库
+        try {
+            MediaStore.Images.Media.insertImage(this.getContentResolver(),
+                    file.getAbsolutePath(), fileName, null);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Bitmap ShotWeb(){
+        mWebView.measure(View.MeasureSpec.makeMeasureSpec(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        mWebView.layout(0, 0, mWebView.getMeasuredWidth(), mWebView.getMeasuredHeight());
+        mWebView.setDrawingCacheEnabled(true);
+        mWebView.buildDrawingCache();
+        Bitmap longImage = Bitmap.createBitmap(mWebView.getMeasuredWidth(),
+                mWebView.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(longImage);  // 画布的宽高和 WebView 的网页保持一致
+        Paint paint = new Paint();
+        canvas.drawBitmap(longImage, 0, mWebView.getMeasuredHeight(), paint);
+        mWebView.draw(canvas);
+        return longImage;
+    }
+
+
+
+
+    private void askPermissions() {
+        int requestCode = 232;
+        String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PermissionChecker.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, permissions, requestCode);
+            }
+        }
+    }
+
+
+
+}
